237f467eb74c87a5326c72c79a64f7b8ac567e38
Philip Seger <seger.philip@gmail.com>
2016-01-06 22:09:15 -0800

    fixing git

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.cpp
+//  HelloWorld
+//
+//  Created by Philip Seger on 1/6/16.
+//  Copyright © 2016 Philip Seger. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello World\n";
+    return 0;
+}
